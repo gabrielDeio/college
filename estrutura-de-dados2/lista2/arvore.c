824feb5a131169dcdbe96317fc39aa8fa3cf65db
gabrielDeio <gabrieldeio40@gmail.com>
2021-10-18 13:16:30 -0300

    Exercicio finalizado

--- estrutura-de-dados2/lista2/arvore.c
+++ estrutura-de-dados2/lista2/arvore.c
@@ -33,42 +33,99 @@ void arv_libera (Arvore* a) {
 
 //========= Exercício 1 - pré-ordem ====
 void print_pre_ordem(Arvore *a){
-   printf("%c\n", a->info);
-	if(a->esq != NULL) print_pre_ordem(a->esq);
-	if(a->dir != NULL) print_pre_ordem(a->dir);
-
+   	if(a == NULL) printf("Arvore Vazia!!\n");
+	else{
+		printf("%c\n", a->info);
+		if(a->esq != NULL) print_pre_ordem(a->esq);
+		if(a->dir != NULL) print_pre_ordem(a->dir);
+	}
 }	
 
 
 //========= Exercício 1 - in-ordem ====
 void print_in_ordem(Arvore *a){
-	if(a->esq != NULL) print_in_ordem(a->esq);
-   printf("%c\n", a->info);
-	if(a->dir != NULL) print_in_ordem(a->dir);
-  
+	if(a == NULL) printf("Arvore Vazia!!\n");
+	else{	
+	
+		if(a->esq != NULL) print_in_ordem(a->esq);
+       		printf("%c\n", a->info);
+		if(a->dir != NULL) print_in_ordem(a->dir);
+  	}
 }
 
 //========= Exercício 1 - pós-ordem ====
 void print_pos_ordem(Arvore *a){
-   if(a->esq != NULL) print_pos_ordem(a->esq);
-   if(a->dir != NULL) print_pos_ordem(a->dir);
-   printf("%c\n", a->info);
+   if(a == NULL) printf("Arvore Vazia!!\n"); 
+   else{
+	if(a->esq != NULL) print_pos_ordem(a->esq);
+   	if(a->dir != NULL) print_pos_ordem(a->dir);
+   	
+	printf("%c\n", a->info);
+   }	 		
 }
 
 //========= Exercício 2 - pertence ====
 int pertence_arv(Arvore *a, char c){
-   if(a->info != c) pertence_arv();
+	if(a == NULL) return -1;
+	else{
+		int answer = 0;
+
+       		if(a->esq != NULL && answer != 1) { 
+			answer = pertence_arv(a->esq, c);
+		}	
+		if(a->dir != NULL && answer != 1){ 
+			answer = pertence_arv(a->dir, c);
+		}
+
+		return a->info == c || answer == 1;
+	}	
 }
 
 //========= Exercício 3 - conta nós ====
+int conta_nos(Arvore *a){
+	if(a == NULL) return -1;
+	else{	
+		int count = 1;
 
+		if(a->esq != NULL){
+			count += conta_nos(a->esq);	
+		}
+		if(a->dir != NULL){
+			count += conta_nos(a->dir);
+		}
+	
+		return count;
+	}	
+}	
 
 //========= Exercício 4 - calcula altura ====
-
+int calcula_altura_arvore(Arvore *a){
+	if(a == NULL) return -1;
+	else{
+		int lh = calcula_altura_arvore(a->esq);
+		int rh = calcula_altura_arvore(a->dir);
+	
+		if(lh < rh) return rh + 1;
+		else return lh + 1;
+	}	
+		
+}	
 
 
 //========= Exercício 5 - conta folhas ====
+int conta_nos_folhas(Arvore *a){
+	if(a == NULL) return -1;
+	else{	
+		int qtdFolha = 0;
 
+		if(a->esq != NULL) qtdFolha += conta_nos_folhas(a->esq);
+		if(a->dir != NULL) qtdFolha += conta_nos_folhas(a->dir);
+
+		if(a->esq == NULL && a->dir == NULL) qtdFolha++;
+
+		return qtdFolha;
+	}	
+}	
 
 
 int main (int argc, char *argv[]) {
@@ -84,10 +141,35 @@ int main (int argc, char *argv[]) {
      )
    );	
 
-   //print_pre_ordem(a);
-   //print_in_ordem(a);
-   print_pos_ordem(a);
-   arv_libera (a);
+   	
+   	printf("\n=====Exercicio 1===========\n");
+	printf("\nPre-Ordem:\n");
+   	print_pre_ordem(a);
+	printf("\nIn-Ordem:\n");	
+	print_in_ordem(a);
+	printf("\nPos-Ordem:\n");
+   	print_pos_ordem(a);
+	printf("\n################################\n\n");
+	
+	printf("\n=====Exercicio 2=============\n");
+       	if (pertence_arv(a, 'e')) printf("A informacao se encontra na arvore!\n");
+	else printf("A informacao nao se encontra na arvore!\n");
+	printf("\n################################\n\n");
+
+
+	printf("\n=====Exercicio 4==============\n");
+	printf("Qtd de nos = %d\n", conta_nos(a));
+	printf("\n##############################\n\n");
+
+	printf("\n=====Exercicio 5==============\n");
+	printf("Altura = %d\n", calcula_altura_arvore(a));
+	printf("\n################################\n\n");
+	
+	printf("\n=====Exercicio 6==============\n");
+	printf("Qtd de folhas = %d\n", conta_nos_folhas(a));
+	printf("\n#################################\n\n");
+		
+	arv_libera (a);
 
    return 0;
 }
